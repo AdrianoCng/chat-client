40f080c3df2940ec916e464a65600b9b920b0828
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-09-30 15:28:24 +0100

    add error input styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,28 @@
-import * as Styled from './styles';
-import React, { forwardRef } from 'react';
+import * as Styled from "./styles";
+import React, { forwardRef } from "react";
 
-interface Props extends React.ComponentProps<'input'> {
-  $fullWidth?: boolean;
+interface Props extends React.ComponentProps<"input"> {
+  fullWidth?: boolean;
+  errorMessage?: string;
 }
 
-const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  return <Styled.InputStyledComponent ref={ref} {...props} />;
-});
+const Input = forwardRef<HTMLInputElement, Props>(
+  ({ fullWidth, errorMessage, ...props }, ref) => {
+    return (
+      <Styled.InputWrapper>
+        <Styled.InputStyledComponent
+          $fullWidth={fullWidth}
+          $hasError={!!errorMessage}
+          ref={ref}
+          {...props}
+        />
+
+        {errorMessage ? (
+          <Styled.ErrorMessage>{errorMessage}</Styled.ErrorMessage>
+        ) : null}
+      </Styled.InputWrapper>
+    );
+  }
+);
 
 export default Input;
--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -1,9 +1,28 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
-export const InputStyledComponent = styled.input<{ $fullWidth?: boolean }>`
+const appear = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+export const InputStyledComponent = styled.input<{
+  $fullWidth?: boolean;
+  $hasError?: boolean;
+}>`
   padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
   font-size: 16px;
   transition: all 150ms ease;
 
@@ -13,18 +32,30 @@ export const InputStyledComponent = styled.input<{ $fullWidth?: boolean }>`
       width: 100%;
     `}
 
+  ${({ $hasError }) =>
+    $hasError &&
+    css`
+      border-color: #ff0000;
+    `}
+
   &:hover:not(:disabled) {
-    border-color: rgba(var(--primary-rgb), 0.5);
+    border-color: #bbb;
   }
 
   &:focus:not(:disabled) {
     outline: none;
-    border-color: rgba(var(--primary-rgb), 0.5);
-    box-shadow: 0 0 5px rgba(var(--primary-rgb), 0.5);
+    border-color: #ccc;
+    box-shadow: 0 0 5px #ccc;
   }
 
   &:disabled {
     background-color: #f0f0f0;
     cursor: not-allowed;
   }
+`;
+
+export const ErrorMessage = styled.span`
+  color: #ff0000;
+  font-size: 1.2rem;
+  animation: ${appear} 250ms ease;
 `;
