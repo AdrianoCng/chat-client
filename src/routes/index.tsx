71ae0d468776b4df0c515944d294c111b215817b
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-11-04 17:59:37 +0000

    remove isLoading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,10 @@ import useAuthProviderContext from "@/contexts/AuthProvider/hooks/useAuthProvide
 import { Navigate } from "react-router-dom";
 
 export default function Login() {
-  const { isLoggedIn, isLoading } = useAuthProviderContext();
+  const { isLoggedIn } = useAuthProviderContext();
 
   if (isLoggedIn) {
     return <Navigate to={"/chat"} />;
-  }
-
-  if (isLoading) {
-    return null;
   }
 
   return <LoginForm />;
